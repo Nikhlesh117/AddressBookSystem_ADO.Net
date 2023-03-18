dea2c14ab9a7671fd8277462e3245ea7e6cc3484
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-03-18 11:10:29 +0530

    Welcome to the Address Book System ADO.Net

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystemADO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the AddressBookSystem ADO.Net");
+        }
+    }
+}
